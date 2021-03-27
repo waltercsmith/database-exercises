3e07bede3b5795d74ce36ff31357908918534149
Walter Smith <walter.c.smith01@gmail.com>
2021-03-27 19:02:53 -0400

    Added another table

--- albums_migration.sql
+++ albums_migration.sql
@@ -11,3 +11,12 @@ CREATE table albums(
     genre varchar(100),
     primary key (id)
 );
+
+
+CREATE TABLE quotes (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        author_first_name VARCHAR(50) DEFAULT 'NONE',
+                        author_last_name  VARCHAR(100) NOT NULL,
+                        content TEXT NOT NULL,
+                        PRIMARY KEY (id)
+);
